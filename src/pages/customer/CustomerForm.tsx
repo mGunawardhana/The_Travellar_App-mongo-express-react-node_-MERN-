bda67e757572f4af05b2d66b86985bd3d83996a6
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-30 22:33:47 +0530

    customer table setup

--- src/pages/customer/CustomerForm.tsx
+++ src/pages/customer/CustomerForm.tsx
@@ -1,12 +1,84 @@
 import React from "react";
 
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Paper,
+} from "@mui/material";
 
 const CustomerForm = () => {
-    return (
-      <div>
+  return (
+    <section className="my-[70px] xl:my-[150px]">
+      <div className="container mx-auto">
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow className="bg-black">
+                <TableCell style={{ color: "#ffffff", fontWeight: "bolder" }}>
+                  ID
+                </TableCell>
+                <TableCell style={{ color: "#ffffff", fontWeight: "bolder" }}>
+                  First Name
+                </TableCell>
+                <TableCell style={{ color: "#ffffff", fontWeight: "bolder" }}>
+                  Last Name
+                </TableCell>
+                <TableCell style={{ color: "#ffffff", fontWeight: "bolder" }}>
+                  Address
+                </TableCell>
+                <TableCell style={{ color: "#ffffff", fontWeight: "bolder" }}>
+                  Contact
+                </TableCell>
+                <TableCell style={{ color: "#ffffff", fontWeight: "bolder" }}>
+                  Email
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>C001</TableCell>
+                <TableCell>Maneesha</TableCell>
+                <TableCell>Gunawardhana</TableCell>
+                <TableCell>Galle</TableCell>
+                <TableCell>071-9054432</TableCell>
+                <TableCell>manee@gmail.com</TableCell>
+              </TableRow>
 
+              <TableRow>
+                <TableCell>C002</TableCell>
+                <TableCell>Sasmitha</TableCell>
+                <TableCell>Gunawardhana</TableCell>
+                <TableCell>Galle</TableCell>
+                <TableCell>071-2344432</TableCell>
+                <TableCell>sas@gmail.com</TableCell>
+              </TableRow>
+
+              <TableRow>
+                <TableCell>C003</TableCell>
+                <TableCell>Asoka</TableCell>
+                <TableCell>Gunawardhana</TableCell>
+                <TableCell>Galle</TableCell>
+                <TableCell>071-9124432</TableCell>
+                <TableCell>asoka@gmail.com</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>C004</TableCell>
+                <TableCell>Dinusha</TableCell>
+                <TableCell>Gunawardhana</TableCell>
+                <TableCell>Galle</TableCell>
+                <TableCell>071-9052332</TableCell>
+                <TableCell>dinu@gmail.com</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
-    );
+    </section>
+  );
 };
 
 export default CustomerForm;
