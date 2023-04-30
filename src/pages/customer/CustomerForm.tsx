61168fc7deb9a337c6c5438432665c8ac252cf56
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-30 23:04:26 +0530

    content update

--- src/pages/customer/CustomerForm.tsx
+++ src/pages/customer/CustomerForm.tsx
@@ -43,7 +43,27 @@ const CustomerForm = () => {
                   required
                 />
               </Stack>
-              
+              <Stack spacing={2} direction="row" sx={{ marginBottom: 2 }}>
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  color="secondary"
+                  label="First Name"
+                  size="small"
+                  fullWidth
+                  required
+                />
+                <TextField
+                  type="text"
+                  variant="outlined"
+                  color="secondary"
+                  label="Last Name"
+                  size="small"
+                  fullWidth
+                  required
+                />
+              </Stack>
+
               <Button variant="outlined" color="secondary" type="submit">
                 Register
               </Button>
