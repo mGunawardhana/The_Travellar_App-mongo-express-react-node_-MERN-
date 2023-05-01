7d2d853dcbf2d327171e63e0c7ec40b72b456019
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-05-01 21:45:21 +0530

    content update

--- src/components/SystemNavMobile/SystemNavMobile.tsx
+++ src/components/SystemNavMobile/SystemNavMobile.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-import {SystemNavData} from "../data";
+import {navigationData} from "../data";
 import {NavLink} from "react-router-dom";
 
 const SystemNavMobile = () => {
     return (
         <ul className="flex flex-col px-6 py-8 gap-y-4">
-            <ul className="flex gap-x-8">
-                <NavLink to={"/customer"}>Customer</NavLink>
-                <NavLink to={"/package"}>Package</NavLink>
-                <NavLink to={"/jeep"}>Vehicle</NavLink>
-                <NavLink to={"/driver"}>Driver</NavLink>
-            </ul>
+                    <li >
+                        <ul className="flex gap-x-8">
+                            <NavLink to={"/customer"}>Customer</NavLink>
+                            <NavLink to={"/package"}>Package</NavLink>
+                            <NavLink to={"/jeep"}>Vehicle</NavLink>
+                            <NavLink to={"/driver"}>Driver</NavLink>
+                        </ul>
+                    </li>
         </ul>
     );
 };
