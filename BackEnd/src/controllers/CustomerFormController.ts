b827b7de6f2eb88c54d5360137065172f1157705
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-05-03 13:47:30 +0530

    introducing customer save method

--- BackEnd/src/controllers/CustomerFormController.ts
+++ BackEnd/src/controllers/CustomerFormController.ts
@@ -1,20 +1,30 @@
 import {Request, RequestHandler, Response} from "express";
+import {Customer} from "../models/Customer";
 
 export default class CustomerFormController {
     createCustomer: RequestHandler = async (
         req: Request,
         res: Response
     ): Promise<Response> => {
-    try{
-        const {customerName} = req.body;
-        let customer = await Category.findOne({customerName: categoryName});
+        try {
+            const {customerName} = req.body;
+            let customer = await Customer.findOne({customerName: customerName});
 
-    }catch (e:unknown){
-        if (e instanceof Error) {
-            return res.status(500).json({message: e.message});
-        } else {
-            return res.status(500).json({message: "unknown error occurred!"});
+            if (!customer) {
+                customer = new Customer({customerName: customerName});
+                customer = await customer.save();
+                return res
+                    .status(200)
+                    .json({message: "New Customer added!", responseDate: customer});
+            } else {
+                return res.status(500).json({message: "Already exists !"});
+            }
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                return res.status(500).json({message: e.message});
+            } else {
+                return res.status(500).json({message: "unknown error occurred!"});
+            }
         }
-    }
-    }
+    };
 }
