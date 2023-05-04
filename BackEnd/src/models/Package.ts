f96551225d04d4abe3b35fef9cfd4bf22990c6da
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-05-04 10:23:10 +0530

    content update

---
+++ BackEnd/src/models/Package.ts
@@ -0,0 +1,42 @@
+import {Document, model, Schema} from "mongoose";
+
+export interface IPackage extends Document {
+    packageID: string;
+    packageName: string;
+    daysHrsCount: string;
+    description: string;
+    offers: number;
+    packageAmount: number;
+}
+
+const PackageSchema = new Schema(
+    {
+        packageID: {
+            type: String,
+            required: true,
+        },
+        packageName: {
+            type: String,
+            required: true,
+        },
+        daysHrsCount: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        offers: {
+            type: Number,
+            required: true,
+        },
+        packageAmount: {
+            type: Number,
+            required: true,
+        },
+    },
+    {timestamps: true}
+);
+
+export const Package = model<IPackage>("Package", PackageSchema);
--- BackEnd/src/routes/index.ts
+++ BackEnd/src/routes/index.ts
@@ -6,6 +6,5 @@ const router:Router = Router();
 
 const url_prefix="/api/v1";
 
-
 router.use(`${url_prefix}/customer`,new CustomerRoutes().getRouter());
 export default router;
