ddd937756ee7c614cb30a3bc1213dd0368fdbcc0
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-05-05 07:47:12 +0530

    driver save methd fixed

---
+++ BackEnd/src/controllers/DriverController.ts
@@ -0,0 +1,28 @@
+import {Request, RequestHandler, Response} from "express";
+import {Driver} from "../models/Driver";
+
+export default class DriverController {
+    createDriver: RequestHandler = async (
+        req: Request,
+        res: Response
+    ): Promise<Response> => {
+        try {
+            const {driverID} = req.body;
+            console.log(req.body);
+            let driver = await Driver.findOne({driverID: driverID});
+            if (!driver) {
+                let driver = new Driver(req.body);
+                let driver01 = await driver.save();
+                return res.json({message: "New Driver added.!", responseData: driver01});
+            } else {
+                return res.status(200).json({message: "Already exists."});
+            }
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                return res.status(500).json({message: error.message})
+            } else {
+                return res.status(500).json({message: "Unknown error occurred!"})
+            }
+        }
+    };
+}
