a3b14f4ccf13e219d9cc23cdd80a5fc98473138b
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-05-10 16:27:46 +0530

    introducing packagebooking properties

---
+++ FrontEnd/src/types/PackageBookinProperties.ts
@@ -0,0 +1,15 @@
+export type PackageBookingProperties = {
+    _id: string;
+    bookingID: string;
+    packageID: string;
+    packageName: string;
+    jeepCode: string;
+    jeepModel: string;
+    jeepPrice: string;
+    offers: number;
+    driverCode: string;
+    driverName: string;
+    customerCode: string;
+    customerName: string;
+    amount: number;
+};
