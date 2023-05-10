043707b3e6e4028068c499d27230ce82174b84df
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-05-10 15:57:21 +0530

    content update

--- BackEnd/src/models/PackageBooking.ts
+++ BackEnd/src/models/PackageBooking.ts
@@ -1,42 +1,33 @@
 import { Document, model, Schema } from "mongoose";
+import { type } from "os";
 
 export interface IPackageBooking extends Document {
-  customerID: string;
-  customerFirstName: string;
-  customerLastName: string;
-  customerAddress: string;
-  customerContact: string;
-  customerEmail: string;
+  bookingID: string;
+  packageID: string;
+  packageName: string;
+  jeepCode: string;
+  jeepModel: string;
+  jeepPrice: string;
+  offers: number;
+  driverCode: string;
+  driverName: string;
+  customerCode: string;
+  customerName: string;
+  amount: number;
 }
 
-const PackageBooking = new Schema(
-  {
-    customerID: {
-      type: String,
-      required: true,
-    },
-    customerFirstName: {
-      type: String,
-      required: true,
-    },
-    customerLastName: {
-      type: String,
-      required: true,
-    },
-    customerAddress: {
-      type: String,
-      required: true,
-    },
-    customerContact: {
-      type: String,
-      required: true,
-    },
-    customerEmail: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
 
-export const Customer = model<IPackageBooking>("packageBooking", PackageBooking);
+const PackageBooking = new Schema(
+    {
+      bookingID: {
+        type: String,
+        required: true,
+      },
+
+    },
+    {timestamps: true}
+);
+export const bookingPackage = model<IPackageBooking>(
+  "packageBooking",
+  PackageBooking
+);
