a035985d461f028a7941404afe1eec722cf4b101
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-05-12 12:04:40 +0530

    introducing payment routings

---
+++ BackEnd/src/routes/Payments.ts
@@ -0,0 +1,29 @@
+import express, {Router} from "express";
+import PaymentController from "../controllers/Payment";
+
+export default class PackageBookingRoutes {
+    private router: Router = express.Router();
+    private paymentController: PaymentController = new PaymentController();
+
+    constructor() {
+        this.configRoutes();
+    }
+
+    public getRouter = (): Router => {
+        return this.router;
+    };
+
+    private configRoutes = (): void => {
+        // POST /api/v1/category
+        this.router.post("/", this.paymentController.createPayment);
+
+        // GET /api/v1/category
+        //this.router.get("/", this.packageController.getAllPackages);
+
+        // DELETE /api/v1/category/:id
+        //this.router.put("/:id", this.packageController.updatePackage);
+
+        // PUT /api/v1/category/:id
+        //this.router.delete("/:id", this.packageController.deletePackage);
+    };
+}
