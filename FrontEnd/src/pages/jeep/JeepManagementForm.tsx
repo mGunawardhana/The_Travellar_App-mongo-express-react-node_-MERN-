6ce2ba42774c21a0b8be1889f7f94a1e8e5e2deb
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-05-13 14:27:55 +0530

    content update

--- FrontEnd/src/pages/jeep/JeepManagementForm.tsx
+++ FrontEnd/src/pages/jeep/JeepManagementForm.tsx
@@ -153,7 +153,25 @@ const JeepManagementForm = () => {
                 .delete(`jeep/${mongoPrimaryKey}`)
                 .then((response) => {
                     getAllJeeps();
-                    alert("Data deleted successfully. ");
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        iconColor: '#ff4757',
+                        backdrop: 'true',
+                        background: '#ffffff',
+                        didOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        },
+                    })
+
+                    // success , error , warning , info , question ,width,color
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Delete Successfully!'
+                    })
                 })
                 .catch((error) => {
                     console.log(error);
